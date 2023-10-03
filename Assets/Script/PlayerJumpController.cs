92e817d91940203fef38a2160c4fa7aebf909fdb
Noto <109500255+Note106569@users.noreply.github.com>
2023-10-03 17:31:52 +0900

    player_jump
    コメントを追加しました。
    インスペクター上にジャンプ力の表示

--- Assets/Script/PlayerJumpController.cs
+++ Assets/Script/PlayerJumpController.cs
@@ -6,9 +6,11 @@ public class PlayerJumpController : MonoBehaviour
 {
     private Rigidbody2D rbody2D;
 
-    private float jumpForce = 450f;
+    [SerializeField]
+    private float jumpForce = 450f; //ジャンプ力
 
-    private int jumpCount = 0;
+    private int jumpCount = 0;　//現在ジャンプ数
+    
 
     void Start()
     {
@@ -17,15 +19,18 @@ public class PlayerJumpController : MonoBehaviour
 
     void Update()
     {
+        
         if (Input.GetKey(KeyCode.Space)&& this.jumpCount < 1)
         {
+            //transform.upで上方向に対して、jumpForceの力を加えます。
             this.rbody2D.AddForce(transform.up * jumpForce);
             jumpCount++;
         }
     }
-
+    //Colliderオブジェクト同士が当たった時、
     private void OnCollisionEnter2D(Collision2D other)
     {
+        //そのオブジェクトの名前がFloorの場合
         if (other.gameObject.name=="Floor")
         {
             jumpCount = 0;
