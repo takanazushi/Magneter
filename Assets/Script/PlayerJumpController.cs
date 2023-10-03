17d8538edcbb77b51200277b491a83469f6837d1
Noto <109500255+Note106569@users.noreply.github.com>
2023-10-03 15:52:14 +0900

    Player_Jump
    プレイヤーのジャンプの処理です

---
+++ Assets/Script/PlayerJumpController.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerJumpController : MonoBehaviour
+{
+    private Rigidbody2D rbody2D;
+
+    private float jumpForce = 450f;
+
+    private int jumpCount = 0;
+
+    void Start()
+    {
+        rbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space)&& this.jumpCount < 1)
+        {
+            this.rbody2D.AddForce(transform.up * jumpForce);
+            jumpCount++;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.name=="Floor")
+        {
+            jumpCount = 0;
+        }
+    }
+}
