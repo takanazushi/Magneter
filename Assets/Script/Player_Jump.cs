d3a7d7b8ab974f202ea2ae433063d0dfdb7f379c
Matsuura Miu <sinikake0519@yahoo.co.jp>
2023-10-06 17:09:07 +0900

    Script名の変更

--- Assets/Script/Player_Jump.cs
+++ Assets/Script/Player_Jump.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerJumpController : MonoBehaviour
+public class Player_Jump : MonoBehaviour
 {
     private Rigidbody2D rbody2D;
 
