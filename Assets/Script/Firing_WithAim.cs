f205d314d7d9339b1c9f4ab676213ea512dedb63
Matsuura Miu <sinikake0519@yahoo.co.jp>
2023-10-31 18:03:33 +0900

    プレイヤーに追尾する弾の処理の修正 弾が弾を発射するバグの修正

--- Assets/Script/Firing_WithAim.cs
+++ Assets/Script/Firing_WithAim.cs
@@ -1,63 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 
 public class Firing_WithAim : MonoBehaviour
 {
-    private Rigidbody2D rb;
-
-    //画面内
-    private bool InField = false;
-
     //Prefabsで複製する物を入れる
     [SerializeField, Header("弾Prefabを入れてください")]
     public GameObject BulletObj;
 
-    //プレイヤーの座標格納
-    [SerializeField, Header("プレイヤーを入れてください")]
-    public Transform target;
-
-    //移動速度
-    [SerializeField, Header("弾の発射スピード")]
-    public float moveSpeed;
-
     void Start()
-    {
-        rb = GetComponent<Rigidbody2D>();
-        //2.5秒後に実行する処理
-        InvokeRepeating("Shot", 2.5f,2.5f);
+    {   
+        StartCoroutine(ShotCoroutine());
     }
 
-    //画面外で消す処理
-    private void OnBecameInvisible()
+    IEnumerator ShotCoroutine()
     {
-        //消す
-        Destroy(rb.gameObject);
-    }
-
-    //画面内で動かす
-    private void OnBecameVisible()
-    {
-        //画面内にいるときtrue
-        InField = true;
-    }
-    void Shot()
-    {
-        //画面内で実行
-        if (InField)
+        while (true)
         {
-            // 弾の方向を自機から敵へ向ける
-            Vector3 bulletDirection = target.position - transform.position;
-            //向き固定
-            bulletDirection.Normalize();
+            // Prefabを実体化
+            Instantiate(BulletObj);
 
-            //複製処理
-            GameObject obj = Instantiate(BulletObj);
-
-            //その向きにスピードを掛ける
-            rb.velocity = bulletDirection * moveSpeed;
-            //10秒後に砲弾を破壊する
-            Destroy(obj, 10.0f);
+            // 1.5秒待機する
+            yield return new WaitForSeconds(1.5f);
         }
     }
+
 }
---
+++ Assets/Script/Turret_Bullet.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Turret_Bullet : MonoBehaviour
+{
+    //プレイヤーの座標格納
+    private Transform target;
+
+    //移動速度
+    [SerializeField, Header("弾の発射スピード")]
+    private float moveSpeed;
+
+    //画面内
+    private bool InField = false;
+
+    private Rigidbody2D rb;
+
+    private Vector3 bulletDirection;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+
+        // 弾の方向を自機から敵へ向ける
+        bulletDirection = target.position - transform.position;
+    }
+
+    //画面内で動かす
+    private void OnBecameVisible()
+    {
+        //画面内にいるときtrue
+        InField = true;
+    }
+
+    //画面外で消す処理
+    private void OnBecameInvisible()
+    {
+        //消す
+        Destroy(rb.gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //画面内で実行
+        if (InField)
+        {
+            //向き固定
+            bulletDirection.Normalize();
+
+            //その向きにスピードを掛ける
+            rb.velocity = bulletDirection * moveSpeed;
+
+            //10秒後に砲弾を破壊する
+            Destroy(gameObject, 10.0f);
+        }
+
+           
+    }
+}
