e56a8aac3a5cf998a1ecbc9f28b30247550c94f1
Matsuura Miu <sinikake0519@yahoo.co.jp>
2023-11-14 13:29:18 +0900

    Lineを引いて移動する床の位置を、外部から設定できるように変更

--- Assets/Script/LineRender.cs
+++ Assets/Script/LineRender.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Line : MonoBehaviour
 {
+    [SerializeField,Header("※リストの最初と最後は同じポジションにすること")]
+    private Transform[] transforms;
+
     //GameObject LineRenderで使用
     void Start()
     {
@@ -12,13 +15,19 @@ public class Line : MonoBehaviour
         // 線の幅
         renderer.SetWidth(0.1f, 0.1f);
         // 頂点の数
-        renderer.SetVertexCount(5);
-        renderer.positionCount = 5;
+        renderer.SetVertexCount(transforms.Length);
+        renderer.positionCount = transforms.Length;
+
+        for(int i = 0; i < transforms.Length; i++)
+        {
+            renderer.SetPosition(i, transforms[i].position);
+        }
+
         // 頂点を設定
-        renderer.SetPosition(0, new Vector3(7,-3,0));
-        renderer.SetPosition(1, new Vector3(7,0,0));
-        renderer.SetPosition(2, new Vector3(-7,0,0));
-        renderer.SetPosition(3, new Vector3(-7,-3,0));
-        renderer.SetPosition(4, new Vector3(7,-3,0));
+        //renderer.SetPosition(0, new Vector3(7,-3,0));
+        //renderer.SetPosition(1, new Vector3(7,0,0));
+        //renderer.SetPosition(2, new Vector3(-7,0,0));
+        //renderer.SetPosition(3, new Vector3(-7,-3,0));
+        //renderer.SetPosition(4, new Vector3(7,-3,0));
     }
 }
