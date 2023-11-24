18e60a2a4f3aae56d0fae25f51b9e84d21a23cfb
yukari <c1217@oic.jp>
2023-11-24 14:37:55 +0900

    動く足場のプログラムを修正

--- Assets/Script/Object/MoveFloor/LineMove/LineRender.cs
+++ Assets/Script/Object/MoveFloor/LineMove/LineRender.cs
@@ -4,30 +4,46 @@ using UnityEngine;
 
 public class Line : MonoBehaviour
 {
-    [SerializeField,Header("※リストの最初と最後は同じポジションにすること")]
+    [SerializeField, Header("巡回フラグ"),
+        Tooltip("巡回する場合チェック")]
+    bool Loop;
+
+
     private Transform[] transforms;
 
     //GameObject LineRenderで使用
-    void Start()
-    {
-        //コンポーネントLineRendererを得る
-        LineRenderer renderer = gameObject.GetComponent<LineRenderer>();
-        // 線の幅
-        renderer.SetWidth(0.1f, 0.1f);
-        // 頂点の数
-        renderer.SetVertexCount(transforms.Length);
-        renderer.positionCount = transforms.Length;
+    //void Start()
+    //{
+    //    int num = 0;
 
-        for(int i = 0; i < transforms.Length; i++)
-        {
-            renderer.SetPosition(i, transforms[i].position);
-        }
+    //    //コンポーネントLineRendererを得る
+    //    LineRenderer renderer = gameObject.GetComponent<LineRenderer>();
+    //    // 線の幅
+    //    renderer.SetWidth(0.1f, 0.1f);
 
-        // 頂点を設定
-        //renderer.SetPosition(0, new Vector3(7,-3,0));
-        //renderer.SetPosition(1, new Vector3(7,0,0));
-        //renderer.SetPosition(2, new Vector3(-7,0,0));
-        //renderer.SetPosition(3, new Vector3(-7,-3,0));
-        //renderer.SetPosition(4, new Vector3(7,-3,0));
-    }
+    //    int len;
+    //    if (Loop)
+    //    {
+    //        len = transforms.Length+1;
+    //    }
+    //    else
+    //    {
+    //        len = transforms.Length;
+    //    }
+
+    //    // 頂点の数
+    //    renderer.positionCount = len;
+
+    //    for(int i = 0; i < transforms.Length; i++)
+    //    {
+    //        renderer.SetPosition(i, transforms[i].position);
+    //    }
+
+    //    //巡回の場合はループさせる
+    //    if (Loop)
+    //    {
+    //        renderer.SetPosition(transforms.Length, transforms[0].position);
+    //    }
+
+    //}
 }
